1b3b29c9578f22ce6002c080cd74a1d8a6c70228
abhilashmusti <59607632+abhilashmusti@users.noreply.github.com>
2020-01-24 15:19:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.use(express.static('./public'))
 app.get('/', (req, res) => {
     res.render('home')
 });
-
-app.listen(5000, () => {
-    console.log('app listen on port 5000')
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`app listen on port ${port}`)
 })
 
 module.exports = app
